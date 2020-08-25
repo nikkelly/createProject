ce75aa2ff4f12f5070e05639d0a6e56cee2965a1
Nik Kelly <nikkelly@outlook.com>
2020-08-25 08:15:45 -0700

    first commit

---
+++ createProject.ps1
@@ -0,0 +1,41 @@
+function createProject() {
+  $headers = @{
+    "accept"        = "application/vnd.github.v3+json"
+    "Authorization" = "Bearer $env:githubToken"
+    "Content-Type"  = "text/plain"
+  }
+  $projectName = Read-Host 'Project Name: '
+  $privateAnswer = Read-Host 'Private Repo? (Y / N)'
+  Switch ($privateAnswer)
+  {
+    Y {Write-Host 'Creating new private repo'; $body = "{
+      `n    `"name`":`"$projectName`",
+      `n    `"private`":true
+      `n}"}
+    N {Write-Host 'Creating new public repo'; $body = "{
+      `n    `"name`":`"$projectName`",
+      `n    `"private`":false
+      `n}"}
+    Default {Write-Host 'Default, creating new private repo'; $body = "{
+      `n    `"name`":`"$projectName`",
+      `n    `"private`":true
+      `n}"}
+  }
+
+  # Create local project folder
+  Set-Location -Path "D:\Git Repos\"
+  mkdir $projectName
+  Set-Location $projectName
+
+  # create remote Github Repo
+  $response = Invoke-RestMethod 'https://api.github.com/user/repos' -Method 'POST' -Headers $headers -Body $body
+  $response | ConvertTo-Json
+
+  # create local repo
+  Write-Output "# testProject" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git remote add origin https://github.com/nikkelly/$projectName
+  git push -u origin master
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# createProject 
+
+## objective
+type 'createProject' to setup project on local machine, create remote GitHub repo and initial commit
+
+## Installation
+Use `notepad $profile` to add this to your PowerShell profile
+```
+Import-module '<path to file>/createProject.ps1' -Force
+```
+
+
+##### Inspired by (Kalle Hallden)[https://youtu.be/7Y8Ppin12r4]
